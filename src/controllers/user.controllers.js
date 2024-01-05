8a7d4e2b3d8dfc572b39b0e66ca442243e8ba8eb
Adharsh <adharshpotharam@gmail.com>
2024-01-06 00:09:05 +0530

    writting down the algorithm

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,19 @@
 import { asyncHandler } from "../utils/async-handler.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message : 'ok'
-    })
+    // get user details from frontend
+    // validation
+    // check if user already exsists with username and email
+    // check for images , check for avatar
+    // upload them to cloudinary, avatar 
+    // create user object - crate entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response if created other wise return error
+    
+    
+    const { fullName , email,username, password}= req.body()
+    console.log("email : ",email)
 })
 
 
